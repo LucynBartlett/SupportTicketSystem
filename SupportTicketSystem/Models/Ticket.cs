9268fdaef809cae8e22b43d5aaea43113adbd3e7
special-chainsaw <lucy@vector-systems.co.uk>
2026-05-19 15:53:09 +0100

    Add project files.

---
+++ SupportTicketSystem/Models/Ticket.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SupportTicketSystem.Models;
+
+public class Ticket
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public string Description { get; set; } = "";
+    public string Priority { get; set; } = "";
+    public string Status { get; set; } = "Open";
+    public bool IsUrgent { get; set; } = false;
+}
